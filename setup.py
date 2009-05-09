46591128250a6a1862f74e89894816ee82612ca4
tasuku <tasuku@d0d07461-0603-4401-acd4-de1884942a52>
2009-05-09 12:05:41 +0000

    Applied patch written on http://d.hatena.ne.jp/stog/20090504#1241451199.
    git-svn-id: http://svn.coderepos.org/share/lang/python/pytc/trunk@33196 d0d07461-0603-4401-acd4-de1884942a52

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 import sys
 from distutils.core import setup, Extension
+import commands
+import re
+import os
 
 if sys.version_info < (2, 3):
   raise Error, "Python 2.3 or later is required"
 
 include_dirs = []
 library_dirs = ['/usr/local/lib']
+
+if sys.platform == 'linux2':
+  os.environ['PATH'] += ":/usr/local/bin:$home/bin:.:..:../.."
+
+  tcinc = commands.getoutput('tcucodec conf -i 2>/dev/null')
+  m = re.search(r'-I([/\w]+)', tcinc)
+  if m:
+    for path in m.groups():
+      include_dirs.append(path)
+    include_dirs = sorted(set(include_dirs), key=include_dirs.index)
+
+  tclib = commands.getoutput('tcucodec conf -l 2>/dev/null')
+  m = re.search(r'-L([/\w]+)', tclib)
+  if m:
+    for path in m.groups():
+      library_dirs.append(path)
+    library_dirs = sorted(set(library_dirs), key=library_dirs.index)
 
 if sys.platform == 'darwin':
   # darwin ports
